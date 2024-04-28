12b7e12ac84d6edc65f1a896064681e7df9fdb87
user <nastastrocilina7@gmail.com>
2024-04-28 06:22:20 +0000

    refatoring

--- informatics/leaning_lang/while_01.c
+++ informatics/leaning_lang/while_01.c
@@ -59,12 +59,13 @@ void equal_degree()
     int N;
     int degree;
     int number = 2;
-    int sq=1;
+    int sq = 1;
+
     scanf("%d", &N);
     while (sq * number < N)
     {
         sq = sq * number;
-        degree=sq
+        degree = sq;
     }
     if (degree * 2 == N || N == 2 || N == 1)
     {
@@ -74,4 +75,45 @@ void equal_degree()
     {
         printf("NO");
     }
+}
+
+// https://informatics.msk.ru/mod/statements/view.php?id=249&chapterid=3061#1
+void equal_degree()
+{
+    int N;
+    int count = 0;
+    int number = 2;
+    int sq = 1;
+
+    scanf("%d", &N);
+    while (sq * number < N)
+    {
+        sq = sq * number;
+        count = count + 1;
+    }
+    if (sq != N)
+    {
+        printf("%d", count + 1);
+    }
+    else
+    {
+        printf("%d", count);
+    }
+}
+
+// https://informatics.msk.ru/mod/statements/view.php?id=249&chapterid=3062#1
+void morning_jog()
+{
+    float x;
+    int count = 1;
+    int y;
+
+    scanf("%e", &x);
+    scanf("%d", &y);
+    while (y - x > 0)
+    {
+        x = x * 1.1;
+        count = count + 1;
+    }
+    printf("%d", count);
 }
