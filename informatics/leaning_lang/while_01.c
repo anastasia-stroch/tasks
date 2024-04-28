6611e41ff91d7fd6e51d340cea96815b37533155
user <nastastrocilina7@gmail.com>
2024-04-28 03:57:17 +0000

    1111

--- informatics/leaning_lang/while_01.c
+++ informatics/leaning_lang/while_01.c
@@ -15,3 +15,63 @@ void square()
         sq = count * count;
     }
 }
+
+// https://informatics.msk.ru/mod/statements/view.php?id=249&chapterid=3058#1
+void divider()
+{
+    int number;
+    int divider = 2;
+    
+    scanf("%d", &number);
+    while (divider * divider <= number && number % divider != 0)
+    {
+        divider++;
+    }
+    
+    if (divider * divider > number)
+        printf("%d", number);
+    else
+        printf("%d", divider);
+}
+
+// https://informatics.msk.ru/mod/statements/view.php?id=249&chapterid=3059#1
+// void degree()
+{
+    int N;
+    int number = 2;
+    int sq = 1;
+
+    scanf("%d", &N);
+    if (N > 0)
+    {
+        printf("%d ", sq);
+    }
+    while (sq * number <= N)
+    {
+        sq = sq * number;
+        printf("%d ", sq);
+    }
+}
+
+// https://informatics.msk.ru/mod/statements/view.php?id=249&chapterid=3060#1
+int main()
+{
+    int N;
+    int degree;
+    int number = 2;
+    int sq=1;
+    scanf("%d", &N);
+    while (sq * number < N)
+    {
+        sq = sq * number;
+        degree=sq
+    }
+    if (degree * 2 == N || N == 2 || N == 1)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
